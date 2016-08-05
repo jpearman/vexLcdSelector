b1fb08bff3b79ec1604c3caa0effc461b173a596
James Pearman <jbpearman@mac.com>
2016-08-04 17:06:44 -0700

    Update license

--- lcdAutonDemo_2.c
+++ lcdAutonDemo_2.c
@@ -1,7 +1,22 @@
 /*-----------------------------------------------------------------------------*/
 /*  LCD autonomous demo 2                                                      */
 /*  Copyright (c) 2013 James Pearman                                           */
-/*  This is unlicensed software - you may modofy and use as you wish           */
+/*                                                                             */
+/*  Licensed under the Apache License, Version 2.0 (the "License");            */
+/*  you may not use this file except in compliance with the License.           */
+/*  You may obtain a copy of the License at                                    */
+/*                                                                             */
+/*      http://www.apache.org/licenses/LICENSE-2.0                             */
+/*                                                                             */
+/*  Unless required by applicable law or agreed to in writing, software        */
+/*  distributed under the License is distributed on an "AS IS" BASIS,          */
+/*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   */
+/*  See the License for the specific language governing permissions and        */
+/*  limitations under the License.                                             */
+/*                                                                             */
+/*  The author can be contacted on the vex forums as jpearman                  */
+/*  or electronic mail using jbpearman_at_mac_dot_com                          */
+/*  Mentor for team 8888 RoboLancers, Pasadena CA.                             */
 /*-----------------------------------------------------------------------------*/
 
 //Competition Control and Duration Settings
